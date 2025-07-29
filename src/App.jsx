b20581c50186bd4cfa34573c5ec5a86a390a9b6f
nana-kwao <nanakwaofreiku@gmail.com>
2025-07-29 07:40:17 +0000

    testing the waters

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return <>
+  <div>
+    <h1>Miles Hotel</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores dolorum veritatis aspernatur autem cum nemo.</p>
+  </div>
+  </>;
 }
 
 export default App;
