77bae39e7186278612238e20f487c1726d542ce3
nana-kwao <nanakwaofreiku@gmail.com>
2025-07-29 07:32:22 +0000

    minor deploy update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/mileshotel",
+  resolve: {
+    alias: {
+      "@img": path.resolve(__dirname, "src/assets/img"),
+      "@fonts": path.resolve(__dirname, "src/assets/fonts"),
+      "@icons": path.resolve(__dirname, "src/assets/icons"),
+    },
+  },
+});
