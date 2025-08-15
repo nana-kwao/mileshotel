754978690e05bf00eac8f13fcdb2747a876756e9
nana-kwao <nanakwaofreiku@outlook.com>
2025-08-15 18:20:21 +0000

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import crypto from "crypto";
+
+if (!globalThis.crypto) {
+  globalThis.crypto = crypto;
+  globalThis.crypto.getRandomValues = (arr) => crypto.randomFillSync(arr);
+}
 
 // https://vite.dev/config/
 export default defineConfig({
